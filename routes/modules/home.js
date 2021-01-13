72cb7aaad59e9b18fa5581225bf9d997f151723c
Lin <mt5718214@gmail.com>
2021-01-13 21:04:57 +0800

    add user register feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -13,6 +15,51 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, passwordCheck } = req.body
+  const errors = []
+
+  if (!(name && email && password && passwordCheck)) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== passwordCheck) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      passwordCheck
+    })
+  }
+
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (!user) {
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(() => res.redirect('/'))
+          .catch(err => console.log(err))
+      }
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        passwordCheck
+      })
+    })
 })
 
 router.get('/logout', (req, res) => {
