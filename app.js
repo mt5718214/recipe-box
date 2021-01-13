b68949de488a3796bbc51d80c4e613b6fd713d6f
Lin <mt5718214@gmail.com>
2021-01-13 20:46:19 +0800

    install connect-flash & create partials template to show messgae

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 
@@ -18,6 +19,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
