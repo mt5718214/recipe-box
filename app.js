a3e7f8156446ca70926a321580be7e1958c2b1b9
Lin <mt5718214@gmail.com>
2021-01-13 14:00:21 +0800

    set up routes & login page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const routes = require('./routes')
 
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -20,9 +21,7 @@ usePassport(app)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const passport = require('passport')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+module.exports = router
