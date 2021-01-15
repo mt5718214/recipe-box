05928f26ca6ea738f97bf03d0fce127bdc47fa33
Lin <mt5718214@gmail.com>
2021-01-15 13:48:51 +0800

    create users seeder

---
+++ seeders/20210113142212-userSeeder.js
@@ -0,0 +1,30 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'root',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'user1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'user2',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
