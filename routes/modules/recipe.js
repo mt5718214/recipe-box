7d13056380ab4129c54b0153a71701358a84589d
Lin <mt5718214@gmail.com>
2021-01-17 14:23:24 +0800

    add create recipe function

--- routes/modules/recipe.js
+++ routes/modules/recipe.js
@@ -16,11 +16,57 @@ router.get('/my-recipe', (req, res) => {
     ]
   })
     .then(user => {
-      console.log(user.toJSON())
       res.render('recipe', {
         user: user.toJSON()
       })
     })
+})
+
+//新增食譜頁面
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+//新增食譜
+router.post('/new', (req, res) => {
+  const { title, description } = req.body
+  const promiseArray = []
+
+  //檢查title,description是否為空值
+  if (!title.trim() || !description.trim()) {
+    req.flash('warning_msg', '標題和描述為必填')
+    return res.redirect('back')
+  }
+
+  return Recipe.create({
+    name: title,
+    description: description,
+    UserId: req.user.id
+  }).then(recipe => {
+    if (req.body.ingredientsItems) {
+      const ingredientsPromise = new Promise((resolve, reject) => {
+        return resolve(Ingredient.create({
+          content: req.body.ingredientsItems,
+          RecipeId: recipe.id
+        }))
+      })
+      promiseArray.push(ingredientsPromise)
+    }
+    if (req.body.stepsItems) {
+      const directionsPromise = new Promise((resolve, reject) => {
+        return resolve(Direction.create({
+          content: req.body.stepsItems,
+          RecipeId: recipe.id
+        }))
+      })
+      promiseArray.push(directionsPromise)
+    }
+
+    return Promise.all(promiseArray)
+      .then(([ingredients, directions]) => {
+        res.redirect('/recipes/my-recipe')
+      })
+  })
 })
 
 module.exports = router
